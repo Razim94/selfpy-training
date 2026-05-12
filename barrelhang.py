df557016ae549c16169ec165071ffa8bd77afbb6
Razi <razi@example.com>
2026-05-12 13:11:29 +0300

    Refactor letter guessing logic

--- barrelhang.py
+++ barrelhang.py
@@ -60,14 +60,14 @@ stage_7 = r"""
     SPLASH!
 """
 
-# Game setup
-
 def guess_letter():
+    """Prompt the user to guess a letter, return it lowercase."""
     letter_guessed = input("Guess a letter: ").lower()
     return letter_guessed
 
 
-def check_valid_input(letter_guessed, old_letters_guessed): 
+def check_valid_input(letter_guessed, old_letters_guessed):
+    """Check if guessed letter is a single English and not guessed before."""
     if (len(letter_guessed) >= 2
          or not str.isalpha(letter_guessed)
          or not str.isascii(letter_guessed)
@@ -79,12 +79,15 @@ def check_valid_input(letter_guessed, old_letters_guessed):
          and letter_guessed.lower() not in old_letters_guessed):
         return True
     
-def try_update_letter_guessed(letter_guessed, old_letters_guessed): 
+def try_update_letter_guessed(letter_guessed, old_letters_guessed):
+    """Use check_valid_input to check if guessed letter is valid,
+    if yes, add to old_letters_guessed and return True,
+    else print X and sorted list of old letters and return False."""
     checked_letter = check_valid_input(letter_guessed, old_letters_guessed)
     if checked_letter == False:
         separator = " -> "
         print("X")
-        print(sorted(separator.join(old_letters_guessed.lower)))
+        print(separator.join(sorted(old_letters_guessed)))
         return False
     elif checked_letter == True:
         old_letters_guessed.append(letter_guessed.lower())
@@ -93,8 +96,15 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed):
 def main():
     old_letters_guessed = []
     letter_guessed = guess_letter()
-    post_check_letter = check_valid_input(letter_guessed, old_letters_guessed)
+    post_check_letter = try_update_letter_guessed(letter_guessed, old_letters_guessed)
     print(post_check_letter)
+# -- Test calls --
+# old_letters = ['a', 'p', 'c', 'f']
+# try_update_letter_guessed('A', old_letters)   # X, sorted list, False
+# try_update_letter_guessed('s', old_letters)   # True
+# try_update_letter_guessed('$', old_letters)   # X, sorted list, False
+# try_update_letter_guessed('d', old_letters)   # True
+
 
 print("Welcome to BarrelHang!")
 print(f"You have 5 attempts to win.")
